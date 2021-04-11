f0a897b3c901939d2a8088a4310d7ba8fc357bf7
Dhruv Gopani <dhruvgopani8@gmail.com>
2021-04-11 10:23:04 +0530

    'final'

---
+++ Public/Akash_css/js/contact.js
@@ -0,0 +1,22 @@
+
+if(document.getElementById('submit')){
+document.getElementById('submit').addEventListener('click',async(e)=>{
+    e.preventDefault();
+    console.log('work')
+    contact={};
+    contact.name=document.getElementById('name').value;
+    contact.email=document.getElementById('email').value;
+    contact.subject=document.getElementById('subject').value;
+    contact.message=document.getElementById('message').value;
+    console.log(contact)
+    const response = await fetch('http://127.0.0.1:3000/sendMail', {
+        method: 'POST',
+        headers: {"Content-type": "application/json; charset=UTF-8"},
+        body:JSON.stringify(contact)
+    })
+    // console.log(response.body)
+    if(response.status==200){
+        alert('Mail sent to Admin , Thank you for connecting with us')
+    }
+})
+}
